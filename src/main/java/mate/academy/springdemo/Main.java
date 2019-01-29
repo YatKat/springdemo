27c5851bc3b7070ec468130ec71c879c851e2a6f
YatKat <lexconsult2008@gmail.com>
2019-01-29 17:24:25 +0200

    spring example: changed wildcard in import

--- src/main/java/mate/academy/springdemo/Main.java
+++ src/main/java/mate/academy/springdemo/Main.java
@@ -1,11 +1,11 @@
 package mate.academy.springdemo;
 
 import mate.academy.springdemo.config.Config;
-import mate.academy.springdemo.dao.PersonDao;
-import mate.academy.springdemo.model.Person;
-import mate.academy.springdemo.service.UserService;
+import mate.academy.springdemo.model.Developer;
+import mate.academy.springdemo.repository.DeveloperRepository;
+import mate.academy.springdemo.service.DeveloperService;
+import mate.academy.springdemo.service.DeveloperServiceImpl;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
@@ -13,17 +13,5 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         System.out.println("Spring Context initialize");
-        UserService userService = context.getBean("userService", UserService.class);
-        userService.printMyName();
-
-        PersonDao personDao = context.getBean(PersonDao.class);
-        Person person = personDao.getById(1);
-        Person person1 = new Person();
-        person1.setFirstName("Cleo");
-        person1.setLastName("Egypt");
-        person1.setAge(10);
-        personDao.createPerson(person1);
-        System.out.println(person.toString());
-        ((ConfigurableApplicationContext) context).close();
     }
 }
