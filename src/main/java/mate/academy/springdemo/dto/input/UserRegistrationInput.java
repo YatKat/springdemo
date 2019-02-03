3ae967b2894eac967ac7d59cdea039ea742970b4
YatKat <lexconsult2008@gmail.com>
2019-02-03 21:05:51 +0200

    login and registration features

---
+++ src/main/java/mate/academy/springdemo/dto/input/UserRegistrationInput.java
@@ -0,0 +1,13 @@
+package mate.academy.springdemo.dto.input;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationInput {
+    private String login;
+    private String password;
+}
---
+++ src/main/webapp/WEB-INF/pages/auth/loggedIn.jsp
@@ -0,0 +1,10 @@
+<%--@elvariable id="user" type="mate.academy.springdemo.dto.input.UserLoginInput"--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Logged In successful</title>
+</head>
+<body>
+Hello, ${user.login}!
+</body>
+</html>
