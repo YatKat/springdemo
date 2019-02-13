0e00f867b7093172c568b4f9ea35877285f02756
YatKat <30606206+YatKat@users.noreply.github.com>
2019-02-13 13:12:45 +0200

    Delete PersonDao.java

--- src/main/java/mate/academy/springdemo/dao/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package mate.academy.springdemo.dao;
-
-import mate.academy.springdemo.model.Person;
-
-public interface PersonDao {
-    Person getById(int id);
-
-    Integer createPerson(Person person);
-}
